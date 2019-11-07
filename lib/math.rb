7cb384ed8a4b7cb8ba0ab3ad82548a1eb448bb58
Cane Rosso <astor@usa.com>
2019-11-07 18:11:07 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
-def addition(num1, num2)
 
+#Remember that the "spec" files are looking for a return value... 
+#the return value of puts is always nil! --> don't use "puts"
+def addition(num1, num2)
+  num1+num2 
 end
 
 def subtraction(num1, num2)
-
+num1-num2
 end
 
 def division(num1, num2)
-
+num1/num2
 end
 
 def multiplication(num1, num2)
-
+num1*num2
 end
 
 def modulo(num1, num2)
-
+remainder = num1.divmod(num2)
+puts remainder
 end
 
 def square_root(num)
-
+#Build the method square_root that finds the square root of num and returns the result
+Math.sqrt(num)
 end
